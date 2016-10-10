420a112fd7a62ba8b8db8a836e23fef7208d31b4
mikelathrop <lathropmike0@gmail.com>
2016-10-09 18:22:38 -0700

    from Acer Laptop
    Push to Github with changes from Acer laptop

---
+++ R Scripts/Estimated Portfolio Volatility Model.R
@@ -0,0 +1,16 @@
+# Estimated Portfolio Volatility Model
+
+# I WILL BE USING ETFs AND MFs SO THEY WILL LIKELY NOT HAVE A LONG ENOUGH HISTORY (NEED TO PROBABLY ONLY GENERATE 1 YR FORECAST)
+
+# Portfolio Variance - Weights of portfolio holdings x covariance matrix x expected returns
+
+#Look at portfolio variance in a number of ways:
+  #Based upon only historical returns (MVO methodology or Portfolio Resampling Methodology)
+  #Based upon modeled estimates
+    # autoregressive methods for modeling the covariance matrix (VAR or GARCH)
+    # portfolio drift for future returns? (what effect will that have from the obversers bias?)
+    # 
+
+# I will be Using ETFs and Mutual Funds (They may not have longer term returns)
+
+# If I use benchmarks, I will have a much larger dataset, but it will not be well representative of the portfolio
---
+++ R Scripts/List of Pertrac Statistics.R
@@ -0,0 +1,39 @@
+# List of Pertrac Stats
+#https://support.pertrac.com/manual/whgdata/whlstt20.htm
+# Formulas from Pertrac 
+#https://support.pertrac.com/PerTrac_User_Manual/module_12_2.html?mw=MjQw&st=MQ==&sct=MA==&ms=AgAAAAAg
+
+
+#Value Added Monthly Index (VAMI)
+#Average Return (Mean)
+#Average Gain ( Gain Mean)
+#Average Loss (Loss Mean)
+#Compound (Geometric) Return
+#Standard Deviation
+#Gain Deviation
+#Loss Deviation
+#Downside Deviation
+#Semi Deviation
+#Sharpe Ratio
+#Sterling Ratio
+#Drawdown
+#Gain to Loss Ratio
+#$Profit to Loss Ratio
+#Correlation Statistics: Alpha & Beta
+#Correlation: Correlation R & R-Squared
+#Kurtosis
+#Tracking Error
+#Treynor Ratio
+#Jensen Alpha
+#Skewness
+#Sortino Ratio
+#Active Premium
+#Information Ratio
+#Up Capture Ratio
+#Down Capture
+#Up Number 
+#Down Number
+#Up Percentage % Ratio
+#Down Percentage % Ratio
+#% Gain Ratio
+#Calmar Ratio
